e6ad319f35f07ee283e0530b453888df4ad0fe68
osamu <46447427+sam-osamu@users.noreply.github.com>
2019-03-23 18:12:20 +0900

    RustDocの公開 #12 ピンポイントじゃなくて、cargo docで生成されたファイル全部上げる必要があった…

---
+++ docs/aliases.js
@@ -0,0 +1,3 @@
+var ALIASES = {};
+ALIASES['kanaria'] = {};
+ALIASES['kanaria'] = {};
---
+++ docs/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['kanaria'] = {"name":"","dirs":[{"name":"converter","dirs":[],"files":["define.rs","factory.rs","mod.rs","one_to_one.rs","one_to_two.rs","two_to_one.rs","utility.rs"]},{"name":"utils","dirs":[],"files":["ascii.rs","kana.rs","mod.rs","width.rs"]}],"files":["char.rs","constants.rs","lib.rs","str.rs"]};
+sourcesIndex['kanaria'] = {"name":"","dirs":[{"name":"converter","dirs":[],"files":["define.rs","factory.rs","mod.rs","one_to_one.rs","one_to_two.rs","two_to_one.rs","utility.rs"]},{"name":"utils","dirs":[],"files":["ascii.rs","kana.rs","mod.rs","width.rs"]}],"files":["char.rs","constants.rs","lib.rs","str.rs"]};
